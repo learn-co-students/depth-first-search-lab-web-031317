023cf69ab2c31bf65a5256c99dbb95abcf53578d
drumnation <davidmieloch@gmail.com>
2017-07-06 16:54:51 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+const depthFirstSearch = (rootNode, vertices, edges) => {
+    let stack = [rootNode]
+    let visited = [rootNode]
+    while (stack.length > 0) {
+        let node = stack.pop()
+        if (node.discovered === null) {
+            node.discovered = true
+            findConnectedNodes(node, vertices, edges).forEach( cn => {
+            visited.push(cn)
+            stack.push(cn)})
+        }
+    }
+    return visited
+}
+
+const findConnectedNodes = (node, vertices, edges) => {
+    let connectedVertices = []
+    let connectedNames = edges.filter( edge => edge.includes(node.name) )
+    let connections = connectedNames.map( cn =>
+        cn.filter( n => n !== node.name)[0])
+        connections.forEach( conn => vertices.forEach( vert => {
+            if (vert.name === conn && vert.discovered === null) connectedVertices.push(vert)
+        }))
+    return connectedVertices
+}
